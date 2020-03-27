2853d2f46531420a7c027021ae5ab0e3806dc5c1
eleven_4u <515503474@qq.com>
2020-03-27 21:51:06 +0800

    add files

---
+++ src/main/java/com/gemini/array/merge/TwoSortedMerge.java
@@ -0,0 +1,45 @@
+package com.gemini.array.merge;
+
+import java.util.Arrays;
+
+/**
+ * todo (用一句话描述该文件做什么)
+ *
+ * @author xiaocuizi
+ * @since 0.0.1 2020/3/27 10:45
+ */
+public class TwoSortedMerge {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+        System.out.printf("nums1=%s", Arrays.toString(nums1));
+    }
+    /**
+     * 输入:
+     * nums1 = [1,2,3,0,0,0], m = 3
+     * nums2 = [2,5,6],       n = 3
+     * <p>
+     * 输出: [1,2,2,3,5,6]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+        }
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+
+    }
+}
